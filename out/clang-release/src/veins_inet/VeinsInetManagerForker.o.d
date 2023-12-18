165c9b1e87e9b5b020809886d2ad3a2980173360
68sus <68sus@looley>
2023-12-18 18:17:03 +1100

    initial one

---
+++ out/clang-release/src/veins_inet/VeinsInetManagerForker.o.d
@@ -0,0 +1,123 @@
+../out/clang-release/src/veins_inet/VeinsInetManagerForker.o: \
+  veins_inet/VeinsInetManagerForker.cc \
+  veins_inet/VeinsInetManagerForker.h veins_inet/veins_inet.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/veins.h \
+  C:/Users/68sus/OneDrive/Desktop/692/project/omnetpp-5.6.2/include\omnetpp.h \
+  ../../inet/src\inet/common/INETDefs.h \
+  ../../inet/src\inet/common/precompiled_release.h \
+  ../../inet/src\inet/common/precompiled.h \
+  ../../inet/src\inet/opp_defines.h ../../inet/src\inet/features.h \
+  ../../inet/src\inet/common/Compat.h \
+  ../../inet/src\inet/common/InitStages.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/mobility/traci/TraCIScenarioManagerForker.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/mobility/traci/TraCIScenarioManager.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/base/utils/Coord.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/base/utils/FWMath.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/base/modules/BaseWorldUtility.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/base/connectionManager/BaseConnectionManager.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/base/utils/AntennaPosition.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/base/connectionManager/NicEntry.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/base/utils/Heading.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/utility/HasLogProxy.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/base/utils/FindModule.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/obstacle/ObstacleControl.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/obstacle/Obstacle.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/world/annotations/AnnotationManager.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/utility/BBoxLookup.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/obstacle/VehicleObstacleControl.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/base/utils/Move.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/obstacle/MobileHostObstacle.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/mobility/traci/TraCIBuffer.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/mobility/traci/TraCIConstants.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/mobility/traci/TraCIColor.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/mobility/traci/TraCIConnection.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/mobility/traci/TraCICoord.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/mobility/traci/TraCICoordinateTransformation.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/mobility/traci/VehicleSignal.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/base/utils/EnumBitset.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/mobility/traci/TraCIRegionOfInterest.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/mobility/traci/TraCILauncher.h \
+  veins_inet/VeinsInetManagerBase.h \
+  C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/utility/SignalManager.h
+
+veins_inet/VeinsInetManagerForker.h:
+
+veins_inet/veins_inet.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/veins.h:
+
+C:/Users/68sus/OneDrive/Desktop/692/project/omnetpp-5.6.2/include\omnetpp.h:
+
+../../inet/src\inet/common/INETDefs.h:
+
+../../inet/src\inet/common/precompiled_release.h:
+
+../../inet/src\inet/common/precompiled.h:
+
+../../inet/src\inet/opp_defines.h:
+
+../../inet/src\inet/features.h:
+
+../../inet/src\inet/common/Compat.h:
+
+../../inet/src\inet/common/InitStages.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/mobility/traci/TraCIScenarioManagerForker.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/mobility/traci/TraCIScenarioManager.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/base/utils/Coord.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/base/utils/FWMath.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/base/modules/BaseWorldUtility.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/base/connectionManager/BaseConnectionManager.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/base/utils/AntennaPosition.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/base/connectionManager/NicEntry.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/base/utils/Heading.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/utility/HasLogProxy.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/base/utils/FindModule.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/obstacle/ObstacleControl.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/obstacle/Obstacle.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/world/annotations/AnnotationManager.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/utility/BBoxLookup.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/obstacle/VehicleObstacleControl.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/base/utils/Move.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/obstacle/MobileHostObstacle.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/mobility/traci/TraCIBuffer.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/mobility/traci/TraCIConstants.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/mobility/traci/TraCIColor.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/mobility/traci/TraCIConnection.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/mobility/traci/TraCICoord.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/mobility/traci/TraCICoordinateTransformation.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/mobility/traci/VehicleSignal.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/base/utils/EnumBitset.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/mobility/traci/TraCIRegionOfInterest.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/mobility/traci/TraCILauncher.h:
+
+veins_inet/VeinsInetManagerBase.h:
+
+C:/Users/68sus/OneDrive/Desktop/veins-master/veins-master/src\veins/modules/utility/SignalManager.h:
